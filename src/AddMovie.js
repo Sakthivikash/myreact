4e4eed566dfa4b4e0627e308c7545fe0cf85de4f
Sakthivikash <sakthivikash70@gmail.com>
2022-02-18 09:17:26 +0530

    Formik vaidation updated for add & edit form.

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,67 +1,202 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useState } from 'react';
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { API } from "./global";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const movieValidationSchema = yup.object({
+  image: yup
+    .string()
+    .required("Why not fill this image? 😉")
+    .min(4, "Need a longer email 😄"),
+  name: yup
+    .string()
+    .required("Why not fill this name? 😉"),
+  summary: yup
+    .string()
+    .required("Why not fill this summary? 😉")
+    .min(20, "Need a longer summary 😄"),
+  direct: yup
+    .string()
+    .required("Why not fill this director name? 😉"),
+  music: yup
+    .string()
+    .required("Why not fill this music director name? 😉"),
+  rating: yup
+    .number()
+    .required("Why not fill this rating? 😉")
+    .min(0)
+    .max(10),
+  trailer: yup
+    .string()
+    .required("Why not fill this trailer 😉")
+    .min(4, "Need a longer  trailer 😄")
+});
+
+
 
 export function AddMovie() {
+const history= useHistory();
+  const formik = useFormik({
+    initialValues: {
+      image: "",
+      name: "",
+      summary: "",
+      direct: "",
+      music:"",
+      rating: "",
+      trailer: ""
+    
+    },
+    validationSchema: movieValidationSchema,
+    onSubmit: (newMovie) => {
+      addMovie(newMovie);
+    },
+  });
 
-  const history= useHistory();
+const addMovie= (newMovie) => {
+  console.log("onSubmit", newMovie);
+  fetch(API, {
+    method: "POST",body: JSON.stringify(newMovie),
+    headers: {"Content-Type": "application/json"}})
+    .then(() => history.push("/movieslist"));
 
-  const [image, setimage] = useState("");
-  const [name, setname] = useState("");
-  const [summary, setsummary] = useState("");
-  const [direct, setdirect] = useState("");
-  const [music, setmusic] = useState("");
-  const [rating, setrating] = useState("");
-  const [trailer, setTrailer] = useState("");
+}
   return (
 <div className='box'>
-<h1 className="title">Add The New Movie</h1>
-    <div className='inputdata'>
+  <h1 className="title">Add The New Movie</h1>
+    <form onSubmit={formik.handleSubmit} className='inputdata'>
       <div className='inputs'>
-        <TextField id="outlined-basic" label="Enter the image URL"
-          onChange={(event) => {
-            setimage(event.target.value); 
-            formik.handleChange;
-            }} 
-            onBlur={formik.handleBlur}
-            variant="outlined" /> <br />
-        <TextField id="outlined-basic" label="Enter the movie name" variant="outlined"
-          onChange={(event) => setname(event.target.value)} /><br />
-        <TextField id="outlined-basic" label="Enter the movie summary" variant="outlined"
-          onChange={(event) => setsummary(event.target.value)} /><br />
-        <TextField id="outlined-basic" label="Enter the director name"
-          onChange={(event) => setdirect(event.target.value)} variant="outlined" /><br />
-        <TextField id="outlined-basic" label='Enter the music director name'
-          onChange={(event) => setmusic(event.target.value)} variant="outlined" />  <br />
-        <TextField id="outlined-basic" label='Enter the rating of the movie'
-          onChange={(event) => setrating(event.target.value)} variant="outlined" /><br />
-        <TextField id="outlined-basic" label='Enter the trailer of the movie'
-          onChange={(event) => setTrailer(event.target.value)} variant="outlined" /><br />
+      <TextField
+          className="outlined-basic"
+          type="text"
+          label="Image"
+          id="image"
+          name="image"
+          onChange={formik.handleChange}
+          value={formik.values.image}
+          onBlur={formik.handleBlur}
+          error={formik.touched.image && formik.errors.image}
+          helperText={
+            formik.touched.image && formik.errors.image
+              ? formik.errors.image
+              : ""
+          }
+        /><br />
+          <TextField
+         className="outlined-basic"
+          type="text"
+          label="Name"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && formik.errors.name}
+          helperText={
+            formik.touched.name && formik.errors.name
+              ? formik.errors.name
+              : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Summary"
+          id="summary"
+          name="summary"
+          onChange={formik.handleChange}
+          value={formik.values.summary}
+          onBlur={formik.handleBlur}
+          error={formik.touched.summary && formik.errors.summary}
+          helperText={
+            formik.touched.summary && formik.errors.summary
+              ? formik.errors.summary: ""
+          }
+        /><br />
+          <TextField
+           className="outlined-basic"
+          type="text"
+          label="Director"
+          id="direct"
+          name="direct"
+          onChange={formik.handleChange}
+          value={formik.values.direct}
+          onBlur={formik.handleBlur}
+          error={formik.touched.direct && formik.errors.direct}
+          helperText={
+            formik.touched.direct && formik.errors.direct
+              ? formik.errors.direct : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Music"
+          id="music"
+          name="music"
+          onChange={formik.handleChange}
+          value={formik.values.music}
+          onBlur={formik.handleBlur}
+          error={formik.touched.music && formik.errors.music}
+          helperText={
+            formik.touched.music && formik.errors.music
+              ? formik.errors.music: ""
+          }
+        />  <br />
+         <TextField
+          className="outlined-basic"
+          type="text"
+          label="Rating"
+          id="rating"
+          name="rating"
+          onChange={formik.handleChange}
+          value={formik.values.rating}
+          onBlur={formik.handleBlur}
+          error={formik.touched.rating && formik.errors.rating}
+          helperText={
+            formik.touched.rating && formik.errors.rating
+              ? formik.errors.rating : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Trailer"
+          id="trailer"
+          name="trailer"
+          onChange={formik.handleChange}
+          value={formik.values.trailer}
+          onBlur={formik.handleBlur}
+          error={formik.touched.trailer && formik.errors.trailer}
+          helperText={
+            formik.touched.trailer && formik.errors.trailer
+              ? formik.errors.trailer : ""
+          }
+      /><br />
 
       </div>
       <div id='addbtn'>
-        <Button className="addbtn" variant="contained" onClick={() => {
-          const newMovie = {
-            name: name,
-            image: image,
-            summary: summary,
-            director: direct,
-            music: music,
-            rating: rating,
-            trailer: trailer
-          };
-          // setList([addmovie, ...list]);
-          fetch(API, {
-            method: "POST",body: JSON.stringify(newMovie),
-            headers: {"Content-Type": "application/json",},})
-            .then(() => history.push("/movieslist"))
-          
-        }}>Add the Movie</Button>
+        <Button className="addbtn" type='submit' variant="contained" >Add the Movie</Button>
       </div>
-     </div>
+     </form>
     {/* <Movieslist list={list}/> */}
 </div>
   );
 }
+// onClick={() => {
+//   const newMovie = {
+//     name: name,
+//     image: image,
+//     summary: summary,
+//     director: direct,
+//     music: music,
+//     rating: rating,
+//     trailer: trailer
+//   };
+//   // setList([addmovie, ...list]);
+ 
+
+  
+// }}
--- src/EditMovie.js
+++ src/EditMovie.js
@@ -1,23 +1,24 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { useState, useEffect } from 'react';
-import { movielist } from './movielist';
+import { useFormik } from "formik";
 import { useParams, useHistory } from "react-router-dom";
 import { API } from "./global";
+import * as yup from "yup";
 
 export function EditMovie() {
 
 const { id } = useParams();
 const [movie, setmovie] = useState(null);
 
-const getMovies = () => {
+
+const getMovies= () => {
   fetch(`${API}/${id}`, 
   {method: "GET",}) // promise
   .then((data) => data.json()) // Response object
   .then((mvs) => setmovie(mvs))
   .catch((err) => console.log(err));
 };
-
   useEffect(() => getMovies(), []);
   
   return (
@@ -25,69 +26,182 @@ const getMovies = () => {
   );
 }
 
+const movieValidationSchema = yup.object({
+  image: yup
+    .string()
+    .required("Why not fill this image? 😉")
+    .min(4, "Need a longer email 😄"),
+  name: yup
+    .string()
+    .required("Why not fill this name? 😉"),
+  summary: yup
+    .string()
+    .required("Why not fill this summary? 😉")
+    .min(20, "Need a longer summary 😄"),
+  direct: yup
+    .string()
+    .required("Why not fill this director name? 😉"),
+  music: yup
+    .string()
+    .required("Why not fill this music director name? 😉"),
+  rating: yup
+    .number()
+    .required("Why not fill this rating? 😉")
+    .min(0)
+    .max(10),
+  trailer: yup
+    .string()
+    .required("Why not fill this trailer 😉")
+    .min(4, "Need a longer  trailer 😄")
+});
+
 function EditMovieForm({movie}){
   const history= useHistory();
+  const formik = useFormik({
+    initialValues: {
+      image: movie.image,
+      name: movie.name,
+      summary: movie.summary,
+      direct: movie.director ,
+      music: movie.music ,
+      rating: movie.rating,
+      trailer: movie.trailer,
+    },
+    validationSchema: movieValidationSchema,
+    onSubmit: (updatedMovie) => {
+      editMovie(updatedMovie);
+    },
+  });
 
-  const [image, setimage] = useState(movie.image);
-  const [name, setname] = useState(movie.name);
-  const [summary, setsummary] = useState(movie.summary);
-  const [direct, setdirect] = useState(movie.director);
-  const [music, setmusic] = useState(movie.music);
-  const [rating, setrating] = useState(movie.rating);
-  const [trailer, setTrailer] = useState(movie.trailer);
+  const editMovie=(updatedMovie) => {
+    fetch(`${API}/${movie.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updatedMovie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => history.push("/movieslist"));
+  }
 
   return(
     <div className='box'>
-      <h1 className="title">Edit The Movie Here</h1>
-      <div className='inputdata'>
-      <div className='inputs'>
-        <TextField id="outlined-basic" label="Enter the image URL" value={image}
-          onChange={(event) => setimage(event.target.value)} variant="outlined" /> <br />
-        <TextField id="outlined-basic" label="Enter the movie name" variant="outlined"
-        value={name}
-          onChange={(event) => setname(event.target.value)} /><br />
-        <TextField id="outlined-basic" label="Enter the movie summary" variant="outlined"
-        value={summary}
-          onChange={(event) => setsummary(event.target.value)} /><br />
-        <TextField id="outlined-basic" label="Enter the director name"
-        value={direct}
-          onChange={(event) => setdirect(event.target.value)} variant="outlined" /><br />
-        <TextField id="outlined-basic" label='Enter the music director name'
-        value={music}
-          onChange={(event) => setmusic(event.target.value)} variant="outlined" />  <br />
-        <TextField id="outlined-basic" label='Enter the rating of the movie'
-        value={rating}
-          onChange={(event) => setrating(event.target.value)} variant="outlined" /><br />
-          <TextField id="outlined-basic" label='Enter the trailer of the movie'
-        value={trailer}
-          onChange={(event) => setTrailer(event.target.value)} variant="outlined" /><br />
-
-      </div>
-      <div id='addbtn'>
-        <Button className="addbtn" variant="contained" color="success" onClick={() => {
-         const updatedMovie = {
-          name: name,
-          image: image,
-          summary: summary,
-          director: direct,
-          music: music,
-          rating: rating,
-          trailer: trailer
-        };
-        // 1. method must be PUT & pass id
-        // 2. body - JSON data
-        // 3. headers - JSON data
-        // After PUT is complete ->  movie to /movies
-        fetch(`${API}/${movie.id}`, {
-          method: "PUT",
-          body: JSON.stringify(updatedMovie),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then(() => history.push("/movieslist"));
-        }}>Edit Movie</Button>
-      </div>
-     </div>
+    <h1 className="title">Add The New Movie</h1>
+      <form onSubmit={formik.handleSubmit} className='inputdata'>
+        <div className='inputs'>
+        <TextField
+          className="outlined-basic"
+          type="text"
+          label="Image"
+          id="image"
+          name="image"
+          onChange={formik.handleChange}
+          value={formik.values.image}
+          onBlur={formik.handleBlur}
+          error={formik.touched.image && formik.errors.image}
+          helperText={
+            formik.touched.image && formik.errors.image
+              ? formik.errors.image
+              : ""
+          }
+        /><br />
+          <TextField
+         className="outlined-basic"
+          type="text"
+          label="Name"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && formik.errors.name}
+          helperText={
+            formik.touched.name && formik.errors.name
+              ? formik.errors.name
+              : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Summary"
+          id="summary"
+          name="summary"
+          onChange={formik.handleChange}
+          value={formik.values.summary}
+          onBlur={formik.handleBlur}
+          error={formik.touched.summary && formik.errors.summary}
+          helperText={
+            formik.touched.summary && formik.errors.summary
+              ? formik.errors.summary: ""
+          }
+        /><br />
+          <TextField
+           className="outlined-basic"
+          type="text"
+          label="Director"
+          id="direct"
+          name="direct"
+          onChange={formik.handleChange}
+          value={formik.values.direct}
+          onBlur={formik.handleBlur}
+          error={formik.touched.direct && formik.errors.direct}
+          helperText={
+            formik.touched.direct && formik.errors.direct
+              ? formik.errors.direct : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Music"
+          id="music"
+          name="music"
+          onChange={formik.handleChange}
+          value={formik.values.music}
+          onBlur={formik.handleBlur}
+          error={formik.touched.music && formik.errors.music}
+          helperText={
+            formik.touched.music && formik.errors.music
+              ? formik.errors.music: ""
+          }
+        />  <br />
+         <TextField
+          className="outlined-basic"
+          type="text"
+          label="Rating"
+          id="rating"
+          name="rating"
+          onChange={formik.handleChange}
+          value={formik.values.rating}
+          onBlur={formik.handleBlur}
+          error={formik.touched.rating && formik.errors.rating}
+          helperText={
+            formik.touched.rating && formik.errors.rating
+              ? formik.errors.rating : ""
+          }
+        /><br />
+          <TextField
+          className="outlined-basic"
+          type="text"
+          label="Trailer"
+          id="trailer"
+          name="trailer"
+          onChange={formik.handleChange}
+          value={formik.values.trailer}
+          onBlur={formik.handleBlur}
+          error={formik.touched.trailer && formik.errors.trailer}
+          helperText={
+            formik.touched.trailer && formik.errors.trailer
+              ? formik.errors.trailer : ""
+          }
+        /><br />
+  
+        </div>
+        <div id='addbtn'>
+          <Button className="addbtn" type='submit' variant="contained" >Add the Movie</Button>
+        </div>
+       </form>
+      {/* <Movieslist list={list}/> */}
   </div>
   )
 }
